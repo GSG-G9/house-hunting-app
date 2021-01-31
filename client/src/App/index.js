1161697db43a7d5c0f03efa4683d5fca7a2436be
mariamisa <maryaam1998@hotmail.com>
2021-01-31 23:16:55 +0200

    add client and server and test folder relates #7

---
+++ client/src/App/index.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>welcome welcome!!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Utils/routes.constant.js
@@ -0,0 +1,4 @@
+
+export const HOME_PAGE = '/';
+export const LOGIN_PAGE = '/login';
+export const SIGNUP_PAGE = '/signup';
