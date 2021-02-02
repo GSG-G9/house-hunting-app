4a57020f7a1003d108785a29ea4bc08f19f37494
mariamisa <maryaam1998@hotmail.com>
2021-02-03 01:13:11 +0200

    add

--- server/app.js
+++ server/app.js
@@ -17,6 +17,8 @@ const middleware = [
 app.use(middleware);
 
 app.use('/api/v1', router);
+app.use(notFound);
+app.use(errorHandler);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(join(__dirname, '..', 'client', 'build')));
@@ -24,8 +26,5 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(join(__dirname, '..', 'client', 'build', 'index.html'));
   });
 }
-
-app.use(notFound);
-app.use(errorHandler);
 
 module.exports = app;
