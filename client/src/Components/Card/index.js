27a27dd9160fd5b0290e32b37eea35548d92a546
yasmeenattallah <yattallah7@gmail.com>
2021-02-03 22:32:57 +0200

    component structure

--- client/src/Components/Card/index.js
+++ client/src/Components/Card/index.js
@@ -1,6 +1,30 @@
 import React from 'react';
+import { Card } from '@material-ui/core';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import house from '../../Utils/images/card/house.png';
 
-function CardComponent() {
-  return <di> test Card Component</di>;
+export default function CardComponent() {
+  return (
+    <Card>
+      <CardActionArea>
+        <CardMedia title="Contemplative Reptile" />
+        <img src={house} alt="house" />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Multi Family home
+          </Typography>
+          <Typography variant="body2" component="p" />
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button>more details</Button>
+        <Button>Learn More</Button>
+      </CardActions>
+    </Card>
+  );
 }
-export default CardComponent;
