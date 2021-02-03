1607d666f44e73bf420dfedb245ad29cf363b240
Muhammad Alabadsa <moh22389@gmail.com>
2021-02-03 10:14:14 +0200

    Add Links to Header Component Relates #58

--- client/src/Components/Header/index.jsx
+++ client/src/Components/Header/index.jsx
@@ -4,6 +4,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -14,6 +15,11 @@ const useStyles = makeStyles(() => ({
     flexGrow: 1,
     color: '#1D3D59',
     fontWeight: 'bold',
+  },
+  linkItem: {
+    '& > a': {
+      paddingLeft: '1rem',
+    },
   },
 }));
 
@@ -26,6 +32,12 @@ export default function index() {
           <Toolbar>
             <Typography variant="h6" className={classes.logo}>
               House Hunting
+            </Typography>
+            <Typography className={classes.linkItem}>
+              <Link href="/">Houses</Link>
+              <Link href="/aboutus">Aboutus</Link>
+              <Link href="/signup">Signup</Link>
+              <Link href="/signin">Signin</Link>
             </Typography>
           </Toolbar>
         </AppBar>
