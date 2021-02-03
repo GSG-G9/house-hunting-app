da93de8cdc1a4f155531203a3b777fa80a5c1971
Muhammad Alabadsa <moh22389@gmail.com>
2021-02-03 15:19:24 +0200

    create LinkItem Component Relates #60

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import Header from '../Components/Header';
-
 import './style.css';
 
 function App() {
   return (
     <div>
-      <Header />
       <h1>welcome form hunting app!</h1>
     </div>
   );
