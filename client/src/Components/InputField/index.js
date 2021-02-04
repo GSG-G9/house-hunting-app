70bcfd6a47309ead8c45bd0e7bde47c440d59c36
AdhamHaisami <adham9633@gmail.com>
2021-02-04 14:27:30 +0200

    edit styles classes Relates #70

--- client/src/Components/InputField/index.js
+++ client/src/Components/InputField/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
-import InputBase from '@material-ui/core/InputBase';
+// import InputBase from '@material-ui/core/InputBase';
 import SearchIcon from '@material-ui/icons/Search';
 import useStyles from './style';
 
@@ -8,7 +8,7 @@ function InputField() {
   const classes = useStyles();
   return (
     <div>
-      <SearchIcon />
+      <SearchIcon className={classes.icon} />
       <TextField variant="standard" label="Search" />
     </div>
   );
--- client/src/Components/InputField/style.js
+++ client/src/Components/InputField/style.js
@@ -1,8 +1,12 @@
 import { makeStyles } from '@material-ui/core/styles';
+import theme from '../../App/theme';
 
 const useStyles = makeStyles({
   root: {
     padding: '0 30px',
+  },
+  icon: {
+    color: theme.palette.primary.light,
   },
 });
 
