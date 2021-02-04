40dc4eaae158f0a46bf9e7e63c220595b24ece14
yasmeenattallah <yattallah7@gmail.com>
2021-02-04 19:44:26 +0200

    edit component Relates #59

--- client/src/Components/CustomButton/index.js
+++ client/src/Components/CustomButton/index.js
@@ -3,14 +3,16 @@ import Button from '@material-ui/core/Button';
 
 import { PropTypes } from 'prop-types';
 
-export default function CustomButton(props) {
-  const { Children } = props;
+export default function CustomButton({ Children, variant, color, event }) {
   return (
-    <Button variant="contained" color="primary">
+    <Button variant={variant} color={color} onClick={event}>
       {Children}
     </Button>
   );
 }
 CustomButton.propTypes = {
   Children: PropTypes.string.isRequired,
+  variant: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  event: PropTypes.func.isRequired,
 };
