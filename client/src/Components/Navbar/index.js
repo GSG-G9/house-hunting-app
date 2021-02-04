3adfa740f25f35462ac187e394b501a248ba3333
Muhammad Alabadsa <moh22389@gmail.com>
2021-02-04 10:45:55 +0200

    Separate the styling code to separate files with name style.js and change the component name Relates #58

--- client/src/Components/Navbar/index.js
+++ client/src/Components/Navbar/index.js
@@ -1,34 +1,12 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import LinkItem from '../LinkItem';
+import useStyles from './style';
 
-const useStyles = makeStyles(() => ({
-  root: {
-    backgroundColor: '#FFFFFF',
-    borderBottom: '3px solid #1D3D59',
-    height: '80px',
-    justifyContent: 'center',
-    alignItems: 'center',
-    boxSizing: 'border-box',
-    '& *': {
-      boxSizing: 'border-box',
-    },
-  },
-  toolBar: {
-    width: '85%',
-  },
-  logo: {
-    flexGrow: 1,
-    color: '#1D3D59',
-    fontWeight: 'bold',
-  },
-}));
-
-export default function index() {
+export default function Navbar() {
   const classes = useStyles();
   return (
     <>
