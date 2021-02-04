813cd41862a6e7e9f0c013a0419fe9759c210c6a
AdhamHaisami <adham9633@gmail.com>
2021-02-04 12:58:19 +0200

    set up material UI for the component Relates #70

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,10 +1,19 @@
+import  React from "react";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "./theme";
+import InputField from "../Components/InputField";
 import './style.css';
 
 function App() {
   return (
-    <div>
-      <h1>welcome form hunting app!</h1>
-    </div>
+    <ThemeProvider theme= {theme}>
+      <CssBaseline/>
+      <InputField/>
+        <div>
+          <h1>welcome form hunting app!</h1>
+        </div>
+    </ThemeProvider>
   );
 }
 
--- client/src/Components/InputField/index.js
+++ client/src/Components/InputField/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import TextField from "@material-ui/core/TextField";
 function InputField() {
   return (
     <div>
-      
+      <p>I am an Item!!</p>
+      <TextField/>
     </div>
   )
 }
