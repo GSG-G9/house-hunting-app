db6ceffc3c00dce0f7409a443ea4a5537c847845
Muhammad Alabadsa <moh22389@gmail.com>
2021-02-04 18:44:05 +0200

    Add new line after third party Relates #60

--- client/src/Components/LinkItem/index.js
+++ client/src/Components/LinkItem/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+
 import useStyles from './style';
 
 function LinkItem({ children, linkUrl, registerClass }) {
