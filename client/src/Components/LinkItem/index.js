bea69267cd7da969278b7a078159750d0789d272
Muhammad Alabadsa <moh22389@gmail.com>
2021-02-04 11:47:30 +0200

    Separate the styling code to separate files with name style.js Relates #60

--- client/src/Components/LinkItem/index.js
+++ client/src/Components/LinkItem/index.js
@@ -1,22 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from '@material-ui/core/Link';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles(() => ({
-  root: {
-    marginLeft: '1.25rem',
-    fontWeight: 'bold',
-    color: '#2F5C84',
-  },
-  signupLink: {
-    color: '#FFFFFF !important',
-    backgroundColor: '#EB9235',
-    padding: '1rem',
-    textAlign: 'center',
-    borderRadius: '3px',
-  },
-}));
+import useStyles from './style';
 
 function LinkItem(props) {
   const { text, linkUrl, bgFilled } = props;
---
+++ client/src/Components/LinkItem/style.js
@@ -0,0 +1,18 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    marginLeft: '1.25rem',
+    fontWeight: 'bold',
+    color: '#2F5C84',
+  },
+  signupLink: {
+    color: '#FFFFFF !important',
+    backgroundColor: '#EB9235',
+    padding: '1rem',
+    textAlign: 'center',
+    borderRadius: '3px',
+  },
+}));
+
+export default useStyles;
