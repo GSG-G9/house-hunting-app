ab3cd0769087f8d53415b8849de1c4829c2770c9
yasmeenattallah <yattallah7@gmail.com>
2021-02-04 11:53:32 +0200

    edit theme file place Relate #80

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-import theme from '../theme';
+import theme from './theme';
 
 function App() {
   return (
