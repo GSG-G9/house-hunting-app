c1692422d627dc0bd70adfe464153d4f71c4b826
yasmeenattallah <yattallah7@gmail.com>
2021-02-06 23:25:57 +0200

    review changes
    relates #22

--- server/controller/routes/user/signup.js
+++ server/controller/routes/user/signup.js
@@ -1,24 +1,22 @@
 const bcrypt = require('bcrypt');
 
-const checkUserByEmail = require('../../../database/queries/user/index');
+const { checkUserByEmail, addUser } = require('../../../database/queries/user');
 const boomify = require('../../../utils/boomify');
-const addUser = require('../../../database/queries/user/index');
 
 const signupController = async (req, res, next) => {
   const { email, password } = req.body;
-  const getEmail = await checkUserByEmail(email);
-  const { rows } = getEmail;
 
   try {
-    if (rows.length > 0) {
-      next(boomify(409, 'user already exists '));
+    const { rows } = await checkUserByEmail(email);
+    console.log(rows, 'rr');
+    if (rows.length) {
+      throw boomify(409, 'user already exists ');
     }
-    const hashedPassword = bcrypt.hash(password, 10);
-    await addUser({ ...req.body, password: hashedPassword });
-
-    await res.status(201).json({ status: 201, msg: 'Welcome' });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // const {rows:[result]} = await addUser({ ...req.body, password: hashedPassword });
+    // res.status(201).json({ status: 201, msg: `Welcome,${} ` });
   } catch (err) {
-    res.status(400).json({ status: 400, error: err.message });
+    next(err);
   }
 };
 module.exports = signupController;
--- server/database/queries/user/addUser.js
+++ server/database/queries/user/addUser.js
@@ -3,7 +3,7 @@ const connection = require('../../config/connection');
 const addUser = ({ username, email, password, mobile }) => {
   const sql = {
     text:
-      'INSERT INTO users SET (username,email,password,mobile) VALUES($1,$2,$3,$4)',
+      'INSERT INTO users  (username,email,password,mobile) VALUES($1,$2,$3,$4) RETURNING username ',
     values: [username, email, password, mobile],
   };
   return connection.query(sql);
