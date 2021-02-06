87a302ea7c185aea081523d4db55b6ec36fac363
yasmeenattallah <yattallah7@gmail.com>
2021-02-06 20:37:01 +0200

    hash password using Bcrypt relates #22

--- server/controller/routes/user/signup.js
+++ server/controller/routes/user/signup.js
@@ -1,15 +1,17 @@
+const bcrypt = require('bcrypt');
+
 const checkUserByEmail = require('../../../database/queries/user/checkUserByEmail');
 const boomify = require('../../../utils/boomify');
 
 const signup = async (req, res, next) => {
   try {
-    const { email } = req.body;
+    const { email, password } = req.body;
     const getEmail = await checkUserByEmail(email);
     const { rows } = getEmail;
     if (rows.length > 0) {
       next(boomify(409, 'user already exists '));
     }
-    res.send('hi');
+    const hashedPassword = bcrypt.hash(password, 10);
   } catch (err) {
     res.status(400).json({ status: 400, error: err.message });
   }
