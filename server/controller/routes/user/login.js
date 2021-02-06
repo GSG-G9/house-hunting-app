20b40d52598be3f42d6c9853e0355e96a4dfef84
AdhamHaisami <adham9633@gmail.com>
2021-02-07 01:09:05 +0200

    delete const secretKey and use it directly from emv Relates 25

--- server/controller/routes/user/login.js
+++ server/controller/routes/user/login.js
@@ -4,20 +4,20 @@ const bcrybt = require('bcrypt');
 const { checkUserByEmail } = require('../../../database/queries/user');
 const boomify = require('../../../utils/boomify');
 
-const secretToken = process.env.SECRET_TOKEN;
-
 const login = async (req, res, next) => {
   const { email, password } = req.body;
   try {
-    const { rows } = await checkUserByEmail(email);
+    const {
+      rows: [check],
+    } = await checkUserByEmail(email);
 
-    if (!rows.length) throw boomify(404, 'User does not exist');
+    if (!check) throw boomify(404, 'User does not exist');
 
-    const match = await bcrybt.compare(password, rows[0].password);
+    const match = await bcrybt.compare(password, check.password);
 
     if (!match) throw boomify(400, 'Invalid username/password');
 
-    const token = await jwt.sign(rows[0].id, secretToken);
+    const token = await jwt.sign(check.id, process.env.SECRET_TOKEN);
 
     res
       .json({
