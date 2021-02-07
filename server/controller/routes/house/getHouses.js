20b6170dc279d89eda3eb16ce10975b5fedbe149
Muhammad Alabadsa <moh22389@gmail.com>
2021-02-07 14:31:19 +0200

    Remove an unusefull if statement Relates #41

--- server/controller/routes/house/getHouses.js
+++ server/controller/routes/house/getHouses.js
@@ -2,9 +2,7 @@ const { getAllHousesData } = require('../../../database/queries/house');
 
 const getAllHouses = async (req, res, next) => {
   try {
-    let { limit, skip } = req.query;
-    if (!limit) limit = 10;
-    if (!skip) skip = 0;
+    const { limit, skip } = req.query;
     const { rows } = await getAllHousesData(limit, skip);
 
     return res.status(200).json({ statusCode: 200, data: rows });
