6513e18ccfb48abc72ededad6b5019310922e9c5
Muhammad Alabadsa <moh22389@gmail.com>
2021-02-07 10:45:32 +0200

    Add sortByTopRating function Relates #91

--- client/src/Pages/Landing/index.js
+++ client/src/Pages/Landing/index.js
@@ -1,10 +1,40 @@
-import React from 'react';
+/* eslint-disable no-undef */
+import React, { useState, useEffect } from 'react';
+import CardContainer from '../../Components/CardContainer';
 
 import useStyles from './style';
 
 function Landing() {
   const classes = useStyles();
-  return <div className={classes.root} />;
+
+  const [houses, setHouses] = useState([]);
+  const [topRatingHouses, setTopRatingHouses] = useState([]);
+
+  const sortByTopRating = (housesData) => {
+    const cloningHouse = [...housesData];
+    const sortingHouse = cloningHouse.sort((a, b) => {
+      if (a.rating > b.rating) {
+        return -1;
+      }
+      if (a.rating < b.rating) {
+        return 1;
+      }
+      return 0;
+    });
+    setTopRatingHouses(sortingHouse);
+  };
+
+  useEffect(() => {
+    setHouses(housesArr);
+    sortByTopRating(houses);
+  }, [houses]);
+
+  return (
+    <div className={classes.root}>
+      <CardContainer houses={houses} />
+      <CardContainer houses={topRatingHouses} />
+    </div>
+  );
 }
 
 export default Landing;
