6d2e4124540059e3534ba8f3f0a64066ac292b8d
Muhammad Alabadsa <moh22389@gmail.com>
2021-02-07 11:47:54 +0200

    Add sortByRecentlyAdded function Relates #91

--- client/src/Pages/Landing/index.js
+++ client/src/Pages/Landing/index.js
@@ -3,12 +3,14 @@ import React, { useState, useEffect } from 'react';
 import CardContainer from '../../Components/CardContainer';
 
 import useStyles from './style';
+import housesArr from '../../houseData';
 
 function Landing() {
   const classes = useStyles();
 
   const [houses, setHouses] = useState([]);
   const [topRatingHouses, setTopRatingHouses] = useState([]);
+  const [recentlyAddedHouses, setRecentlyAddedHouses] = useState([]);
 
   const sortByTopRating = (housesData) => {
     const cloningHouse = [...housesData];
@@ -24,15 +26,31 @@ function Landing() {
     setTopRatingHouses(sortingHouse);
   };
 
+  const sortByRecentlyAdded = (housesData) => {
+    const cloningHouse = [...housesData];
+    const sortingHouse = cloningHouse.sort((a, b) => {
+      if (a.created_at > b.created_at) {
+        return -1;
+      }
+      if (a.created_at < b.created_at) {
+        return 1;
+      }
+      return 0;
+    });
+    setRecentlyAddedHouses(sortingHouse);
+  };
+
   useEffect(() => {
     setHouses(housesArr);
     sortByTopRating(houses);
+    sortByRecentlyAdded(houses);
   }, [houses]);
 
   return (
     <div className={classes.root}>
       <CardContainer houses={houses} />
       <CardContainer houses={topRatingHouses} />
+      <CardContainer houses={recentlyAddedHouses} />
     </div>
   );
 }
