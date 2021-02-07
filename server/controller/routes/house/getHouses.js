8ee9e37c33382647083357f0a72aa0be8ddc6956
Muhammad Alabadsa <moh22389@gmail.com>
2021-02-07 14:17:15 +0200

    Move all code in trycatch block Relates #41

--- server/controller/routes/house/getHouses.js
+++ server/controller/routes/house/getHouses.js
@@ -1,11 +1,10 @@
 const { getAllHousesData } = require('../../../database/queries/house');
 
 const getAllHouses = async (req, res, next) => {
-  let { limit, skip } = req.query;
-  if (!limit) limit = 10;
-  if (!skip) skip = 0;
-
   try {
+    let { limit, skip } = req.query;
+    if (!limit) limit = 10;
+    if (!skip) skip = 0;
     const { rows } = await getAllHousesData(limit, skip);
 
     return res.status(200).json({ statusCode: 200, data: rows });
