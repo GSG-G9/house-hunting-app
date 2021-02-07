b715633642f8e1812d386d4bcb2ada866cf6474c
Muhammad Alabadsa <moh22389@gmail.com>
2021-02-07 21:01:09 +0200

    Fetch data from the api on /api/v1/houses endpoint Relates #91

--- client/src/Pages/Landing/index.js
+++ client/src/Pages/Landing/index.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-undef */
 import React, { useState, useEffect } from 'react';
-import CardContainer from '../../Components/CardContainer';
+import axios from 'axios';
 
+import CardContainer from '../../Components/CardContainer';
 import useStyles from './style';
-import housesArr from '../../houseData';
 
 function Landing() {
   const classes = useStyles();
@@ -11,6 +11,7 @@ function Landing() {
   const [houses, setHouses] = useState([]);
   const [topRatingHouses, setTopRatingHouses] = useState([]);
   const [recentlyAddedHouses, setRecentlyAddedHouses] = useState([]);
+  const [errorMsg, setErrorMsg] = useState('');
 
   const sortByTopRating = (housesData) => {
     const cloningHouse = [...housesData];
@@ -40,14 +41,45 @@ function Landing() {
     setRecentlyAddedHouses(sortingHouse);
   };
 
+  async function fetchingData(source, limit = 6, skip = 0) {
+    try {
+      const { data } = await axios.get(
+        `/api/v1/houses?limit=${limit}&skip=${skip}`,
+        {
+          cancelToken: source.token,
+        }
+      );
+      return data;
+    } catch (error) {
+      if (axios.isCancel(err)) {
+        return setErrorMsg('axios request cancelled');
+      }
+      return setErrorMsg(error.message);
+    }
+  }
+
   useEffect(() => {
-    setHouses(housesArr);
+    const cancelToken = axios.CancelToken;
+    const source = cancelToken.source();
+    fetchingData(source)
+      .then((res) => res)
+      .then(({ data }) => {
+        setHouses(data);
+      })
+      .catch((err) => setErrorMsg(err));
+    return () => {
+      source.cancel('axios request cancelled');
+    };
+  }, []);
+
+  useEffect(() => {
     sortByTopRating(houses);
     sortByRecentlyAdded(houses);
   }, [houses]);
 
   return (
     <div className={classes.root}>
+      {errorMsg && <div>{errorMsg}</div>}
       <CardContainer houses={houses} />
       <CardContainer houses={topRatingHouses} />
       <CardContainer houses={recentlyAddedHouses} />
