52cb30be408a9fc2463d443b320e7793c43c9512
mariam isa <47949938+mariamisa@users.noreply.github.com>
2021-02-09 01:03:57 +0200

    Update style.js

--- client/src/Components/Footer/style.js
+++ client/src/Components/Footer/style.js
@@ -8,7 +8,8 @@ const useStyles = makeStyles((theme) => ({
     width: '100%',
     height: '40px',
     backgroundColor: theme.palette.primary.dark,
-
+    position: 'absolute',
+    bottom: 0,
     '& div': {
       display: 'flex',
       justifyContent: 'center',
