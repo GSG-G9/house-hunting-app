57d6760a3d19a85bd31baeb6634f218bc88756db
yasmeenattallah <yattallah7@gmail.com>
2021-02-09 13:43:12 +0200

    handle enter not found location Relates #32

--- server/controller/routes/house/getHouseByLocation.js
+++ server/controller/routes/house/getHouseByLocation.js
@@ -4,11 +4,16 @@ const getHouseByLocation = async (req, res, next) => {
   try {
     const { location } = req.query;
     const { rows } = await getHouseLocation({ location });
-
-    res.json({
-      status: 200,
-      data: rows,
-    });
+    if (rows.length > 0) {
+      res.json({
+        status: 400,
+        data: rows,
+      });
+    } else
+      res.json({
+        status: 200,
+        data: `No Houses Found in ${location}`,
+      });
   } catch (err) {
     next(err);
   }
