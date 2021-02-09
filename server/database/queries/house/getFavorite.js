6b07893f6a2adca8497d4fbe531ce1c186f35972
AdhamHaisami <adham9633@gmail.com>
2021-02-09 17:03:02 +0200

    refactore DB query Relates #24

--- server/database/queries/house/getFavorite.js
+++ server/database/queries/house/getFavorite.js
@@ -3,7 +3,7 @@ const connection = require('../../config/connection');
 const getFavoriteHouses = (userId) => {
   const sql = {
     text:
-      'SELECT  houses.*    FROM favorites INNER JOIN users ON users.id = favorites.user_id INNER JOIN houses ON houses.id = favorites.house_id WHERE favorites.user_id =  $1 ',
+      'SELECT h.*, u.* FROM favorites AS f INNER JOIN users AS u ON u.id = f.user_id INNER JOIN houses AS h ON h.id = f.house_id WHERE f.user_id = $1 ',
     values: [userId],
   };
   return connection.query(sql);
