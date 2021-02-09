d1e3edc4d396ea3a49289a23ac57bcf5fe079476
yasmeenattallah <yattallah7@gmail.com>
2021-02-10 00:13:08 +0200

    axios request data and response in card container
    Relates #104

--- client/src/Pages/Houses/index.js
+++ client/src/Pages/Houses/index.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Axios from 'axios';
+
+import Button from '../../Components/Button';
+import CardContainer from '../../Components/CardContainer';
 
 function SearchPage() {
-  return <div>Hello from search page</div>;
+  const [houses, setHouses] = useState([]);
+  const [error, setError] = useState();
+  const handleData = async () => {
+    try {
+      const location = 'Rafah';
+      const {
+        data: { data },
+      } = await Axios.get(`api/v1/houses/${location}`);
+
+      setHouses(data);
+    } catch (err) {
+      setError(err);
+    }
+  };
+  return (
+    <div>
+      Hello from search page {handleData}
+      <Button onClick={handleData}>hi</Button>
+      <CardContainer houses={houses} />
+    </div>
+  );
 }
 
 export default SearchPage;
