d786cbdc8c3e91e753138d022266601f66773dab
AdhamHaisami <adham9633@gmail.com>
2021-02-09 23:40:08 +0200

    test Relates #24

--- server/controller/routes/house/getFavoriteList.js
+++ server/controller/routes/house/getFavoriteList.js
@@ -2,14 +2,15 @@ const { getFavoriteHouses } = require('../../../database/queries/house');
 
 const getFavoriteList = async (req, res, next) => {
   try {
+    console.log(req.userId);
     const { rows } = await getFavoriteHouses(req.userId);
 
-    return res.status(200).json({
+    res.status(200).json({
       statusCode: 200,
       data: rows,
     });
   } catch (err) {
-    return next(err);
+    next(err);
   }
 };
 
--- server/database/config/fakeData.sql
+++ server/database/config/fakeData.sql
@@ -21,16 +21,15 @@ INSERT INTO
   houses (user_id, location_id, title, description, room_num, bathroom_num, category, price, area)
 VALUES
   (1, 3, 'house with 3 rooms', 'Nulla ngilla non. Nullam.', 3, 2, 'appartment', 100, 100),
-  (3, 2, 'house with 4 rooms', 'Nulla ngilla non. Nullam.', 2, 2, 'appartment', 100, 100),
-  (1, 1, 'house with 5 rooms', 'Nulla ngilla non. Nullam.', 5, 3, 'appartment', 150, 200),
-  (1, 3, 'house with 6 rooms', 'Nulla ngilla non. Nullam.', 3, 2, 'appartment', 80, 120),
+  (5, 2, 'house with 4 rooms', 'Nulla ngilla non. Nullam.', 2, 2, 'appartment', 100, 100),
+  (3, 1, 'house with 5 rooms', 'Nulla ngilla non. Nullam.', 5, 3, 'appartment', 150, 200),
+  (4, 3, 'house with 6 rooms', 'Nulla ngilla non. Nullam.', 3, 2, 'appartment', 80, 120),
   (2, 3, 'house with 56 rooms', 'Nulla ngilla non. Nullam.', 5, 3, 'appartment', 180, 170);
 
 INSERT INTO
   favorites (user_id, house_id)
 VALUES
   (1, 1),
-  (1, 3),
-  (1, 2),
-  (2, 1),
-  (2, 3);
+  (3, 2),
+  (4, 1),
+  (1, 3);
