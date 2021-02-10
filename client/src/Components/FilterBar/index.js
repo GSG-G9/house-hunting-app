65b16653682b081446b0a80e8a1810d6084080c9
yasmeenattallah <yattallah7@gmail.com>
2021-02-10 17:48:13 +0200

    make filter common component and pass it into houses and handle changes in every select Relates #104

---
+++ client/src/Components/FilterBar/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import { func, string } from 'prop-types';
+
+import Slider from '@material-ui/core/Slider';
+
+import Input from '../Input';
+
+function Filter({ onChange, handlePrice }) {
+  return (
+    <>
+      <InputLabel htmlFor="location">Location </InputLabel>
+      <Select
+        id="location"
+        onChange={onChange}
+        name="location"
+        label="location"
+      >
+        <MenuItem value="Gaza" name="Gaza">
+          Gaza
+        </MenuItem>
+        <MenuItem value="Khanyunis">Khanyunis</MenuItem>
+        <MenuItem value="ALwosta">ALwosta</MenuItem>
+        <MenuItem value="Rafah">Rafah</MenuItem>
+        <MenuItem value="North">North</MenuItem>
+      </Select>
+      <InputLabel htmlFor="catagories"> Catagories </InputLabel>
+      <Select id="catagories" onChange={onChange} name="catagories">
+        <MenuItem value="Apartment">Apartment</MenuItem>
+        <MenuItem value="Roof">Roof</MenuItem>
+        <MenuItem value="Classis">Classic</MenuItem>
+        <MenuItem value="Studio">Studio</MenuItem>
+      </Select>
+      <InputLabel>Rooms</InputLabel>
+      <Input type="number" name="rooms" onChange={onChange} />
+      <InputLabel>Price </InputLabel>
+      <Slider
+        name="price"
+        onChange={handlePrice}
+        valueLabelDisplay="auto"
+        aria-labelledby="range-slider"
+        min={100}
+        max={500}
+      />
+    </>
+  );
+}
+Filter.propTypes = {
+  onChange: func.isRequired,
+  value: string.isRequired,
+  handlePrice: func.isRequired,
+};
+
+export default Filter;
--- client/src/Pages/Houses/index.js
+++ client/src/Pages/Houses/index.js
@@ -3,16 +3,39 @@ import Axios from 'axios';
 
 import CardContainer from '../../Components/CardContainer';
 import Search from '../../Components/SearchBar';
+import Filter from '../../Components/FilterBar';
 
 function SearchPage() {
   const [houses, setHouses] = useState([]);
+  const [location, setLocation] = useState();
+  const [catagories, setCatagories] = useState();
+  const [rooms, setRooms] = useState(0);
+  const [price, setPrice] = useState();
   const [error, setError] = useState();
+  const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+      case 'location':
+        setLocation(value);
+        break;
+      case 'catagories':
+        setCatagories(value);
+        break;
+      case 'rooms':
+        setRooms(value);
+        break;
+
+      default:
+    }
+  };
+  const handlePrice = (event, val) => {
+    setPrice(val);
+  };
   const handleData = async () => {
     try {
-      const location = 'Gaza';
+      const Location = 'Gaza';
       const {
         data: { data },
-      } = await Axios.get(`api/v1/houses/${location}`);
+      } = await Axios.get(`api/v1/houses/${Location}`);
 
       setHouses(data);
     } catch (err) {
@@ -22,6 +45,7 @@ function SearchPage() {
   return (
     <div>
       <Search onClick={handleData} />
+      <Filter onChange={handleChange} handlePrice={handlePrice} />
       <CardContainer houses={houses} />
     </div>
   );
