bc386770df6c1d8d57b39ea6f6560cad01fbf4cb
AdhamHaisami <adham9633@gmail.com>
2021-02-10 14:07:39 +0200

    import typography and customButton Relates #108

--- client/src/Pages/FavoriteList/index.js
+++ client/src/Pages/FavoriteList/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import { DataGrid } from '@material-ui/data-grid';
+import Typography from '@material-ui/core/Typography';
+
+import CustomButton from '../../Components/Button';
 
 function Favorite() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Typography>Hello From favorite page</Typography>
+      <CustomButton>Compare</CustomButton>
     </div>
   );
 }
