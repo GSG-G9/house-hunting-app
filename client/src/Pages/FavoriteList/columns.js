ab5de88467e8ff3e013c9bb814c6aa6ee94671c4
AdhamHaisami <adham9633@gmail.com>
2021-02-11 01:39:11 +0200

    build the favorite page, with some styles Relates #108

---
+++ client/src/Pages/FavoriteList/columns.js
@@ -0,0 +1,31 @@
+export default [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'title', headerName: 'Title', width: 130 },
+  { field: 'description', headerName: 'Description', width: 160 },
+  { field: 'category', headerName: 'Type', width: 130 },
+
+  {
+    field: 'room_num',
+    headerName: 'rooms',
+    type: 'number',
+    width: 90,
+  },
+  {
+    field: 'bathroom_num',
+    headerName: 'Bathrooms',
+    type: 'number',
+    width: 90,
+  },
+  {
+    field: 'price',
+    headerName: 'Price',
+    type: 'number',
+    width: 90,
+  },
+  {
+    field: 'area',
+    headerName: 'Area',
+    type: 'number',
+    width: 90,
+  },
+];
--- client/src/Pages/FavoriteList/index.js
+++ client/src/Pages/FavoriteList/index.js
@@ -1,15 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 
 import { DataGrid } from '@material-ui/data-grid';
 import Typography from '@material-ui/core/Typography';
 
 import CustomButton from '../../Components/Button';
+import columns from './columns';
+import useStyles from './style';
 
 function Favorite() {
+  const classes = useStyles();
+
+  const [houses, setHouses] = useState([]);
+  const [errorMsg, setErrorMsg] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchHouses = async (isCurrent) => {
+    try {
+      setIsLoading(true);
+      const {
+        data: { data },
+      } = await Axios('api/v1/favorite');
+      console.log(data);
+      if (isCurrent) {
+        setHouses(data);
+      }
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setErrorMsg(error);
+    }
+  };
+
+  useEffect(() => {
+    let isCurrent = true;
+    fetchHouses(isCurrent);
+    return () => {
+      isCurrent = false;
+    };
+  }, []);
+
   return (
-    <div>
-      <Typography>Hello From favorite page</Typography>
-      <CustomButton>Compare</CustomButton>
+    <div className={classes.root}>
+      <Typography className={classes.title}>My Favorites House</Typography>
+      <CustomButton
+        className={classes.button}
+        variant="contained"
+        color="secondary"
+      >
+        Compare
+      </CustomButton>
+      <div style={{ height: 400, width: '80%' }}>
+        <DataGrid
+          rows={houses}
+          columns={columns}
+          pageSize={5}
+          checkboxSelection
+          loading={isLoading}
+        />
+      </div>
     </div>
   );
 }
