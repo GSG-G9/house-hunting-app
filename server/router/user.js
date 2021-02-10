e2073b8dfc123a86a36139bff3b9271f43420e83
AdhamHaisami <adham9633@gmail.com>
2021-02-10 12:35:26 +0200

    add space to user routes file Relates #24

--- server/router/user.js
+++ server/router/user.js
@@ -3,4 +3,5 @@ const router = require('express').Router();
 const { logoutController } = require('../controller/routes/user');
 
 router.get('/logout', logoutController);
+
 module.exports = router;
