cbf31bfabb352cbc9dac2ff7206b6385b73b839d
AdhamHaisami <adham9633@gmail.com>
2021-02-12 01:46:13 +0200

    clean file from console log functions Relates #114

--- server/controller/routes/user/isAuth.js
+++ server/controller/routes/user/isAuth.js
@@ -6,11 +6,9 @@ const authController = async (req, res, next) => {
     const { token } = req.cookies;
     const { userId } = await verifying(token);
     req.userId = userId;
-    const request = req;
     return res.json({
       userId,
       isAuth: true,
-      request,
     });
   } catch (error) {
     return next(boomify(401, 'unautherized'));
