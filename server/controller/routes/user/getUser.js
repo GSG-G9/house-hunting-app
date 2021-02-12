78725d91eabeb9949b1015107f04e6c584b85309
Muhammad Alabadsa <moh22389@gmail.com>
2021-02-12 18:40:27 +0200

    Handle user not fount error Relates #116

--- server/controller/routes/user/getUser.js
+++ server/controller/routes/user/getUser.js
@@ -1,9 +1,15 @@
+const boomify = require('../../../utils/boomify');
 const { getUserData } = require('../../../database/queries/user');
 
 const getUser = async (req, res, next) => {
   try {
     const { userId } = req;
     const { rows } = await getUserData(userId);
+
+    if (rows.length === 0) {
+      throw boomify(404, 'User Not Found!');
+    }
+
     return res.json({ data: rows[0] });
   } catch (error) {
     return next(error);
