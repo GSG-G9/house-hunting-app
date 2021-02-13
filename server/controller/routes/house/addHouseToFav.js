1aaf553cdb30d0fb8dd7c6c975e965d3eb64c6dd
yasmeenattallah <yattallah7@gmail.com>
2021-02-13 11:58:07 +0200

    get/Add House To Favorite List Relates #26

---
+++ server/controller/routes/house/addHouseToFav.js
@@ -0,0 +1,7 @@
+const addHouseToFav = (req, res) => {
+  res.json({
+    status: 200,
+    message: 'hi,from favorite houses',
+  });
+};
+module.exports = addHouseToFav;
--- server/utils/jwtFunctions.js
+++ server/utils/jwtFunctions.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const JWT = require('jsonwebtoken');
 
 const verifying = (token) =>
