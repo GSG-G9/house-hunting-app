dd89a6ef3073dd7e59da94546aabb4ea4f35a796
AdhamHaisami <adham9633@gmail.com>
2021-02-14 23:51:07 +0200

    edit update user route Relates #35

--- server/controller/routes/user/updateUser.js
+++ server/controller/routes/user/updateUser.js
@@ -1,9 +1,13 @@
 const { updateUser } = require('../../../database/queries');
+const { verifying } = require('../../../utils/jwtFunctions');
 
 const updateUserController = async (req, res, next) => {
-  const { username, email, address, mobile } = req.body;
   try {
-    await updateUser({ username, email, address, mobile });
+    const { username, email, address, mobile } = req.body;
+    const { token } = req.cookies;
+    const { userId } = await verifying(token);
+
+    await updateUser({ userId, username, email, address, mobile });
     res.status(200).json({ status: 200, message: 'user updated Successfully' });
   } catch (error) {
     next(error);
