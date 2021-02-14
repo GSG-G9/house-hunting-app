2275c6137f68d9e3ea3bd482a5e4cd3bd89502d6
AdhamHaisami <adham9633@gmail.com>
2021-02-14 15:09:09 +0200

    build schema for updating user info Relates #35

---
+++ server/middleware/validation/updateUser.js
@@ -0,0 +1,28 @@
+const { object, string, ref } = requir('yub');
+
+const boomify = require('../../utils/boomify');
+
+const updateUserValidation = async (req, res, next) => {
+  try {
+    const { username, email, mobile, address } = req.body;
+    const schema = object().shape({
+      username: string(),
+      email: string().email(),
+      mobile: string().min(9),
+      address: string(),
+    });
+    await schema.validate(
+      {
+        username,
+        email,
+        mobile,
+        address,
+      },
+      { abortEalry: false }
+    );
+  } catch (error) {
+    next(boomify(400, error.errors[0]));
+  }
+};
+
+module.exports = updateUserValidation;
--- server/router/user.js
+++ server/router/user.js
@@ -3,6 +3,6 @@ const router = require('express').Router();
 const { logoutController } = require('../controller/routes/user');
 
 router.get('/logout', logoutController);
-router.put('/user/:id');
+router.patch('/users');
 
 module.exports = router;
