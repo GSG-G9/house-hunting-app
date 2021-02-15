2a608437f97a76693b8a6c24f0caa68a1b3e5dfb
mariamisa <maryaam1998@hotmail.com>
2021-02-15 19:06:05 +0200

    add

--- client/src/Pages/Layout/index.js
+++ client/src/Pages/Layout/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { func } from 'prop-types';
+
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import Header from '../../Components/Navbar';
 import Footer from '../../Components/Footer';
+import AuthContext from '../../Context/AuthContext';
 
 function Layout({ children }) {
+  const { authLoading } = useContext(AuthContext);
+  console.log(authLoading);
   return (
     <>
-      <Header />
-      {children}
-      <Footer />
+      {authLoading ? (
+        <CircularProgress size={25} color="secondary" />
+      ) : (
+        <>
+          <Header />
+          {children}
+          <Footer />
+        </>
+      )}
     </>
   );
 }
