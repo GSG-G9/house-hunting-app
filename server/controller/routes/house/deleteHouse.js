0cd4c0d798e6821a6edf51e4adc0a59e8b483abb
yasmeenattallah <yattallah7@gmail.com>
2021-02-16 01:15:03 +0200

    add db query and pass it to controller func Relates #29

--- server/controller/routes/house/deleteHouse.js
+++ server/controller/routes/house/deleteHouse.js
@@ -1,8 +1,18 @@
+const { deleteHouse } = require('../../../database/queries/house');
+const boomify = require('../../../utils/boomify');
+
 const deleteHouseController = async (req, res, next) => {
   try {
-    await res.json({
+    const { houseId } = req.params;
+    const { userId } = req;
+    const { rows } = await deleteHouse({ houseId, userId });
+
+    if (!rows.length) {
+      throw boomify(404, 'House Not Found');
+    }
+    return res.json({
       status: 200,
-      message: 'hi from delete house page',
+      message: 'House deleted successfully',
     });
   } catch (err) {
     return next(err);
