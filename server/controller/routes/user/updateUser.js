7ce276616726a1a80ed5a05f9c9beadb41985142
AdhamHaisami <adham9633@gmail.com>
2021-02-15 12:10:05 +0200

    refactore contoller Relates #35

--- server/controller/routes/user/updateUser.js
+++ server/controller/routes/user/updateUser.js
@@ -1,11 +1,9 @@
 const { updateUser } = require('../../../database/queries');
-const { verifying } = require('../../../utils/jwtFunctions');
 
 const updateUserController = async (req, res, next) => {
   try {
     const { username, email, address, mobile } = req.body;
-    const { token } = req.cookies;
-    const { userId } = await verifying(token);
+    const { userId } = req;
 
     await updateUser({ userId, username, email, address, mobile });
     res.status(200).json({ status: 200, message: 'user updated Successfully' });
