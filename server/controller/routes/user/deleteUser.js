d362dcb1bec368b1521e3e635413feca8ec277ca
AdhamHaisami <adham9633@gmail.com>
2021-02-15 22:41:24 +0200

    build the delete-account controller Relates #34

--- server/controller/routes/user/deleteUser.js
+++ server/controller/routes/user/deleteUser.js
@@ -1,6 +1,16 @@
 const deleteUser = require('../../../database/queries/user');
 const boomify = require('../../../utils/boomify');
 
-const deleteUserController = (req, res, next) => {};
+const deleteUserController = async (req, res, next) => {
+  try {
+    const { userId } = req;
+    await deleteUser({ userId });
+    return res
+      .clearCookie('token')
+      .json({ statusCode: 200, message: 'Account deleted successfully' });
+  } catch (error) {
+    return next(error);
+  }
+};
 
 module.exports = deleteUserController;
