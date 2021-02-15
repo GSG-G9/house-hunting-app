6456f932c4264ad323fc1b60319a813e93866b31
AdhamHaisami <adham9633@gmail.com>
2021-02-15 11:57:40 +0200

    add dialog Relates #133

---
+++ client/src/Pages/UpdateUserInfo/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+function UpdateUser() {
+  const [open, setOpen] = useState(false);
+  const handleClick = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <div>
+      <Button>Update info</Button>
+      <Dialog />
+      Hello from Update user
+    </div>
+  );
+}
+
+export default UpdateUser;
