f316f0fac63e409b2615e2fbb241a83d024b0a45
AdhamHaisami <adham9633@gmail.com>
2021-02-15 21:53:19 +0200

    refactore update controller Relates #35

--- server/controller/routes/user/updateUser.js
+++ server/controller/routes/user/updateUser.js
@@ -1,10 +1,21 @@
-const { updateUser } = require('../../../database/queries/user');
+const {
+  updateUser,
+  checkUserByEmail,
+} = require('../../../database/queries/user');
 const boomify = require('../../../utils/boomify');
 
 const updateUserController = async (req, res, next) => {
   try {
     const { username, email, address, mobile } = req.body;
     const { userId } = req;
+
+    const { rows: row } = await checkUserByEmail({ email });
+
+    if (row[0].id !== userId) {
+      console.log(row.id, userId, 'ggggg');
+      console.log(row, 'llllllllll');
+      throw boomify(409, 'Email already exists');
+    }
 
     const { rows } = await updateUser({
       userId,
@@ -16,8 +27,12 @@ const updateUserController = async (req, res, next) => {
     if (!rows.length) {
       throw boomify(500, 'faild to update user info');
     }
-    return res.json({ statusCode: 200, message: 'user updated Successfully' });
+    return res.json({
+      statusCode: 200,
+      message: 'user updated Successfully',
+    });
   } catch (error) {
+    console.log(error);
     return next(error);
   }
 };
