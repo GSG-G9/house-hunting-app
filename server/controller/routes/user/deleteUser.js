560807cfc742024ea2965911c406e90be6917adc
AdhamHaisami <adham9633@gmail.com>
2021-02-15 22:43:38 +0200

    refactore delete-account controller Relates #34

--- server/controller/routes/user/deleteUser.js
+++ server/controller/routes/user/deleteUser.js
@@ -4,7 +4,10 @@ const boomify = require('../../../utils/boomify');
 const deleteUserController = async (req, res, next) => {
   try {
     const { userId } = req;
-    await deleteUser({ userId });
+    const { rows } = await deleteUser({ userId });
+    if (!rows.length) {
+      throw boomify(500, 'Something went wrong!');
+    }
     return res
       .clearCookie('token')
       .json({ statusCode: 200, message: 'Account deleted successfully' });
