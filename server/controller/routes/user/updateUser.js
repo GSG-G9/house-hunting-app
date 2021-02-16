ff5286f6e282818c3bdca11f5e1265beabb9a049
AdhamHaisami <adham9633@gmail.com>
2021-02-16 03:01:32 +0200

    edit DB query Relates #35

--- server/controller/routes/user/updateUser.js
+++ server/controller/routes/user/updateUser.js
@@ -1,33 +1,15 @@
-const {
-  updateUser,
-  checkUserByEmail,
-} = require('../../../database/queries/user');
-const boomify = require('../../../utils/boomify');
+const { updateUser } = require('../../../database/queries/user');
 
 const updateUserController = async (req, res, next) => {
   try {
-    const { username, email, address, mobile } = req.body;
+    const { username, address, mobile } = req.body;
     const { userId } = req;
-
-    const {
-      rows: [data],
-    } = await checkUserByEmail({ email });
-
-    const userData = data || { id: -1 };
-
-    if (userData.id !== -1 && userData.id !== userId) {
-      throw boomify(409, 'Email already exists');
-    }
-    const { rows } = await updateUser({
+    await updateUser({
       userId,
       username,
-      email,
       address,
       mobile,
     });
-    if (!rows.length) {
-      throw boomify(500, 'failed to update user info');
-    }
     return res.json({
       statusCode: 200,
       message: 'user updated Successfully',
