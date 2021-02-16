b8a77205421416b35a1e259973476755a401f9a0
yasmeenattallah <yattallah7@gmail.com>
2021-02-16 15:03:38 +0200

    separate table in component Relates #148

---
+++ client/src/Components/Table/style.js
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  img: {
+    width: 150,
+  },
+}));
+
+export default useStyles;
--- client/src/Pages/Profile/Houses/index.js
+++ client/src/Pages/Profile/Houses/index.js
@@ -4,15 +4,9 @@ import Axios from 'axios';
 
 import Typography from '@material-ui/core/Typography';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableRow from '@material-ui/core/TableRow';
-import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Edit';
 import Alert from '@material-ui/lab/Alert';
 
-import Button from '../../../Components/Button';
+import Table from '../../../Components/Table';
 import useStyles from './style';
 
 function Houses() {
@@ -58,40 +52,11 @@ function Houses() {
     <div className={classes.root}>
       <Typography variant="h2">My Houses</Typography>
       {loading && <CircularProgress size={25} color="primary" />}
-      {error && <Alert severity="error">{error}</Alert>}
-      <Table>
-        <TableBody>
-          {houses.length ? (
-            houses.map((house) => (
-              <TableRow align="right">
-                <TableCell>
-                  <img className={classes.img} src={house.image} alt="house " />
-                </TableCell>
-                <TableCell>
-                  {house.area} m <sup>2</sup>
-                </TableCell>
-                <TableCell>{house.price} $ </TableCell>
-                <TableCell>{house.created_at.split(' ')[0]}</TableCell>
-                <TableCell>
-                  <EditIcon color="primary" />
-                </TableCell>
-                <TableCell>
-                  <Button
-                    onClick={() => {
-                      handleDelete(house.id);
-                    }}
-                    color="primary"
-                  >
-                    <DeleteIcon color="primary" />
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))
-          ) : (
-            <Alert severity="info">no houses added yet </Alert>
-          )}
-        </TableBody>
-      </Table>
+      {error ? (
+        <Alert severity="error">{error}</Alert>
+      ) : (
+        <Table houses={houses} onClick={handleDelete} />
+      )}
     </div>
   );
 }
