0045e5854ffe109cd3a10f47bbce02cf72273ba1
mariam isa <47949938+mariamisa@users.noreply.github.com>
2021-02-17 21:57:16 +0200

    Update PrivateRoute.js
    fix refresh

--- client/src/Components/Route/PrivateRoute.js
+++ client/src/Components/Route/PrivateRoute.js
@@ -1,18 +1,12 @@
 import React, { useContext } from 'react';
-import { Redirect, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
-
-import { LOGIN_PAGE } from '../../Utils/routes.constant';
 
 import AuthContext from '../../Context/AuthContext';
 
 function PrivateRoute({ component: Component, ...rest }) {
-  const { isAuth } = useContext(AuthContext);
-  return (
-    <Route {...rest}>
-      {isAuth ? <Component /> : <Redirect to={{ pathname: LOGIN_PAGE }} />}
-    </Route>
-  );
+  const { isAuth, authLoading } = useContext(AuthContext);
+  return <Route {...rest}>{isAuth && !authLoading && <Component />}</Route>;
 }
 PrivateRoute.propTypes = {
   component: PropTypes.func.isRequired,
