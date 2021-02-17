2a2fdce1085298e64e8fded8b0ab3527604665c6
AdhamHaisami <adham9633@gmail.com>
2021-02-17 22:33:55 +0200

    edite update validation schema Relates #133

--- client/src/Utils/validations/updateUser.js
+++ client/src/Utils/validations/updateUser.js
@@ -1,9 +1,9 @@
 import { object, string, number } from 'yup';
 
 const validationSchema = object({
-  username: string().min(4),
-  mobile: number().min(9),
-  address: string(),
+  username: string().min(4).required(),
+  mobile: number().min(9).required(),
+  address: string().required(),
 });
 
 export default validationSchema;
